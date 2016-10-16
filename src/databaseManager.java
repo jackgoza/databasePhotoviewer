e59771c5168b1f996bf512c639a7c12ec960f357
Jack G <jgkp9@umkc.edu>
2016-10-16 15:32:45 -0500

    INIT

---
+++ src/databaseManager.java
@@ -0,0 +1,154 @@
+
+import java.sql.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author jack
+ */
+public class databaseManager {
+
+    private static Connection con;
+    private static Statement stmt;
+
+    private static void openConnection() {
+	String url = "jdbc:mysql://kc-sce-appdb01.kc.umkc.edu/jgkp9?allowMultiQueries=true";
+	String userID = "jgkp9";
+	String password = "java";
+
+	try {
+	    Class.forName("com.mysql.jdbc.Driver");
+	}
+	catch (java.lang.ClassNotFoundException e) {
+	    System.out.println(e);
+	    System.exit(0);
+	}
+
+	try {
+	    con = DriverManager.getConnection(url, userID, password);
+	    con.setAutoCommit(false);
+	    stmt = con.createStatement();
+	}
+	catch (SQLException s) {
+	    System.err.println("Error opening connection");
+	    s.printStackTrace();
+	}
+    }
+
+    private void initializeTable() {
+	try {
+	    openConnection();
+
+	    String createTable = "create table Photos "
+		    + "(id INT NOT NULL AUTO_INCREMENT,"
+		    + " pictnum INT NOT NULL, date VARCHAR(24),"
+		    + " description VARCHAR(256), photo LONG VARBINARY NULL,"
+		    + " PRIMARY KEY (id))";
+
+	    stmt.executeUpdate(createTable);
+	    cleanup();
+	}
+	catch (SQLException e) {
+	    System.err.println("Error initing table");
+	    e.printStackTrace();
+	}
+
+    }
+
+    public int getTableSize() {
+	try {
+	    openConnection();
+
+	    String sizeString = "SELECT COUNT(*) FROM Photos";
+
+	    ResultSet rs;
+
+	    rs = stmt.executeQuery(sizeString);
+
+	    return (rs.getInt("COUNT(*)"));
+
+	}
+	catch (SQLException sizeE) {
+	    System.err.println("Error retrieving size of table");
+	    sizeE.printStackTrace();
+	    return -1;
+	}
+
+    }
+
+    public void addPhotoToDatabase(databasePhoto newPhoto) throws SQLException {
+
+	openConnection();
+	PreparedStatement addStatement = con.prepareStatement(
+		"INSERT INTO photos (pictnum, date, description, photo) VALUES (?,?,?,?)"
+	);
+
+	addStatement.setInt(1, newPhoto.photoNumber);
+	addStatement.setString(2, newPhoto.date);
+	addStatement.setString(3, newPhoto.description);
+	addStatement.setBytes(4, newPhoto.imageArray);
+
+	addStatement.executeUpdate();
+
+	cleanup();
+
+    }
+
+    public databasePhoto getNewPhoto(int index) throws SQLException {
+	openConnection();
+
+	PreparedStatement getPhotoStatement = con.prepareStatement(
+		"SELECT * FROM photos WHERE pictnum = ?"
+	);
+
+	getPhotoStatement.setInt(1, index);
+	ResultSet rs;
+	rs = getPhotoStatement.executeQuery();
+	ResultSetMetaData rsmd = rs.getMetaData();
+	databasePhoto newPhoto = new databasePhoto();
+
+	newPhoto.photoNumber = rs.getInt("pictnum");
+	newPhoto.date = rs.getString("date");
+	newPhoto.description = rs.getString("description");
+	newPhoto.imageArray = rs.getBytes("photo");
+
+	cleanup();
+
+	return newPhoto;
+
+    }
+
+    public void deletePhoto(int index) throws SQLException {
+	openConnection();
+
+	PreparedStatement deletePhotoStatement = con.prepareStatement(
+		"DELETE FROM photos WHERE pictnum = ?"
+	);
+
+	deletePhotoStatement.setInt(1, index);
+
+	deletePhotoStatement.executeUpdate();
+
+	PreparedStatement deleteIndexFixer = con.prepareStatement(
+		"UPDATE Photos SET pictnum=pictnum-1 WHERE pictnum > ?"
+	);
+
+	deletePhotoStatement.setInt(1, index);
+
+	deletePhotoStatement.executeUpdate();
+
+	cleanup();
+
+    }
+
+    public void cleanup() throws SQLException {
+
+	stmt.close();
+	con.close();
+    }
+
+}
---
+++ src/photoViewerGUI.java
@@ -0,0 +1,397 @@
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.sql.SQLException;
+import javax.accessibility.Accessible;
+import javax.swing.*;
+import javax.swing.filechooser.*;
+
+/**
+ *
+ * @author jack
+ */
+public class photoViewerGUI extends JFrame implements ActionListener, Serializable, Accessible {
+
+    Container mainWindow;
+    JPanel controlPane;
+    JMenuBar topBar = null;
+    JMenu fileMenu = null;
+    JMenuItem saveMenuItem, exitMenuItem, browseMenuItem, maintainMenuItem;
+    JMenu viewMenu = null;
+    JTextField currPageText;
+    JTextArea descriptionTextArea, totalPageText;
+    JLabel imageLabel = null;
+    JPanel buttonPane;
+    JTextField dateTextField;
+    JButton nextButton, prevButton, deleteButton, saveButton, addButton;
+    private int maxIndex;
+    private int currIndex;
+    databaseManager db;
+    databasePhoto photo;
+
+    public photoViewerGUI() // Constructor
+    {
+
+	// Set title to string passed in
+	super("Photo viewer");
+	mainWindow = getContentPane();
+
+	databaseManager db = new databaseManager();
+
+	imageLabel = new JLabel("", SwingConstants.CENTER);
+	JScrollPane scrollPane = new JScrollPane(imageLabel);
+
+	controlPane = new JPanel();
+	controlPane.setLayout(new BoxLayout(controlPane, BoxLayout.PAGE_AXIS));
+
+	createMenus();
+
+	createInfoSpace();
+
+	createButtons();
+
+	mainWindow.add(controlPane, BorderLayout.SOUTH);
+
+	mainWindow.add(scrollPane);
+
+	this.setMinimumSize(getSize());
+
+	addWindowListener(new WindowAdapter() {
+	    public void windowClosing(WindowEvent e) {
+		System.exit(0);
+	    }
+	});
+
+    }
+
+    void changeImage() {
+	try {
+	    byte[] rawImage = db.getNewPhoto(currIndex).imageArray;
+	    if (rawImage != null) {
+		ImageIcon image = new ImageIcon(rawImage);
+		imageLabel.setIcon(image);
+	    }
+	}
+	catch (SQLException e) {
+	    System.err.println("Error loading new photo");
+	    e.printStackTrace();
+	}
+    }
+
+    private void createMenus() {
+	// Create menu system and fill with wonderous sub-menus
+
+	topBar = new JMenuBar();
+	fileMenu = new JMenu("File");
+	topBar.add(fileMenu);
+	saveMenuItem = new JMenuItem("Save");
+	exitMenuItem = new JMenuItem("Exit");
+	fileMenu.add(saveMenuItem);
+
+	/*
+
+	saveMenuItem.addActionListener(new ActionListener() {
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+		try {
+		    FileOutputStream fileOut = new FileOutputStream("photolib");
+		    ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+		    objectOut.writeObject(images);
+		    objectOut.close();
+		    fileOut.close();
+		}
+		catch (SQLException ioe) {
+		    System.err.println("SQL error. Database not updated.");
+		}
+	    }
+	});
+
+	 */
+	fileMenu.add(exitMenuItem);
+
+	exitMenuItem.addActionListener(new ActionListener() {
+	    @Override
+	    public void actionPerformed(ActionEvent e
+	    ) {
+		int dialogButton = JOptionPane.YES_NO_OPTION;
+		int dialogResult = JOptionPane.showConfirmDialog(null, "Are you sure you want to quit? Changes may not be saved!", "Warning", dialogButton);
+		if (dialogResult == JOptionPane.YES_OPTION) {
+		    System.exit(0);
+		}
+	    }
+	});
+
+	viewMenu = new JMenu("View");
+	browseMenuItem = new JMenuItem("Browse");
+
+	browseMenuItem.addActionListener(
+		new ActionListener() {
+	    @Override
+	    public void actionPerformed(ActionEvent e
+	    ) {
+		buttonPane.setVisible(false);
+		descriptionTextArea.setEditable(false);
+		dateTextField.setEditable(false);
+	    }
+	}
+	);
+	maintainMenuItem = new JMenuItem("Maintain");
+
+	maintainMenuItem.addActionListener(
+		new ActionListener() {
+	    @Override
+	    public void actionPerformed(ActionEvent e
+	    ) {
+		buttonPane.setVisible(true);
+		descriptionTextArea.setEditable(true);
+		dateTextField.setEditable(true);
+	    }
+	}
+	);
+	topBar.add(viewMenu);
+
+	viewMenu.add(browseMenuItem);
+
+	viewMenu.add(maintainMenuItem);
+
+	this.setJMenuBar(topBar);
+    }
+
+    private void createInfoSpace() {
+	JPanel descriptionPane = new JPanel();
+	descriptionPane.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+	JLabel descriptionLabel = new JLabel("Description:");
+	descriptionTextArea = new JTextArea(4, 20);
+	descriptionTextArea.setEditable(false);
+
+	descriptionPane.add(descriptionLabel);
+	descriptionPane.add(descriptionTextArea);
+
+	JPanel datePane = new JPanel();
+//		datePane.setLayout(new FlowLayout(FlowLayout.LEFT));
+//		datePane.setLayout(new BoxLayout(datePane, BoxLayout.LINE_AXIS));
+
+	JLabel dateLabel = new JLabel("Date:");
+	dateLabel.setPreferredSize(new Dimension(descriptionLabel.getPreferredSize().width, dateLabel.getPreferredSize().height));
+	dateTextField = new JTextField();
+	dateTextField.setEditable(false);
+	dateTextField.setPreferredSize(new Dimension(100, 25));
+	datePane.add(dateLabel);
+	datePane.add(dateTextField);
+	//datePane.add(Box.createHorizontalGlue());
+
+	deleteButton = new JButton("Delete");
+	deleteButton.addActionListener(new ActionListener() {
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+		try {
+		    db.deletePhoto(currIndex);
+
+		}
+		catch (SQLException ee) {
+		    System.err.println("Couldn't delete photo");
+		}
+	    }
+	});
+
+	saveButton = new JButton("Save");
+
+	/*
+	saveButton.addActionListener(new ActionListener() {
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+		images.get(imageNumber).setDescription(descriptionTextArea.getText());
+		images.get(imageNumber).setDate(dateTextField.getText());
+
+	    }
+	});
+	 */
+	addButton = new JButton("Add Items");
+	addButton.addActionListener(new ActionListener() {
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+		JFileChooser chooser = new JFileChooser();
+		chooser.changeToParentDirectory();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter(
+			"JPG & GIF Images", "jpg", "gif");
+		chooser.setFileFilter(filter);
+		int returnVal = chooser.showOpenDialog(null);
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+//		    addPhoto(chooser.getSelectedFile().getAbsolutePath());
+		}
+	    }
+	});
+
+	buttonPane = new JPanel();
+	buttonPane.add(deleteButton);
+	buttonPane.add(saveButton);
+	buttonPane.add(addButton);
+
+	JPanel leftRightPane = new JPanel();
+	leftRightPane.setLayout(new BorderLayout());
+	leftRightPane.add(datePane, BorderLayout.WEST);
+	leftRightPane.add(buttonPane, BorderLayout.EAST);
+	buttonPane.setVisible(false);
+	controlPane.add(descriptionPane);
+	controlPane.add(leftRightPane);
+
+    }
+
+    private void createButtons() {
+	Container bottomPanel = new JPanel();
+	Container bottomPanelWest = new JPanel();
+	Container bottomPanelEast = new JPanel();
+	FlowLayout flleft = new FlowLayout(FlowLayout.LEFT, 5, 20);
+	FlowLayout flright = new FlowLayout(FlowLayout.RIGHT, 5, 20);
+	bottomPanelWest.setLayout(new FlowLayout(FlowLayout.LEFT));
+	bottomPanelEast.setLayout(new FlowLayout(FlowLayout.RIGHT));
+	bottomPanel.setLayout(new BoxLayout(bottomPanel, BoxLayout.LINE_AXIS));
+
+	// Create buttons and text fields
+	currPageText = new JTextField("1");
+	totalPageText = new JTextArea();
+
+	totalPageText.setText(String.valueOf(maxIndex));
+
+	prevButton = new JButton("<Prev");
+	nextButton = new JButton("Next>");
+	prevButton.setEnabled(false);
+	if (maxIndex <= 1) {
+	    nextButton.setEnabled(false);
+	}
+
+	/*
+	JSlider zoomSlide = new JSlider(JSlider.HORIZONTAL, 0, 100, 50);
+	zoomSlide.addChangeListener(new ChangeListener() {
+	    public void stateChanged(ChangeEvent e) {
+		System.out.println("Value : "
+			+ ((JSlider) e.getSource()).getValue());
+	    }
+	});
+	 */
+	// Init action listeners to check for button presses
+	prevButton.addActionListener(new ActionListener() {
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+		prevButtonAction();
+	    }
+	});
+	nextButton.addActionListener(new ActionListener() {
+	    @Override
+	    public void actionPerformed(ActionEvent e) {
+		nextButtonAction();
+	    }
+	});
+
+	bottomPanelWest.add(currPageText);
+	bottomPanelWest.add(totalPageText);
+	bottomPanelWest.add(prevButton);
+	bottomPanelWest.add(nextButton);
+	// bottomPanelEast.add(zoomSlide);
+
+	bottomPanel.add(bottomPanelWest);
+	bottomPanel.add(Box.createHorizontalGlue());
+	bottomPanel.add(bottomPanelEast);
+
+	controlPane.add(bottomPanel);
+
+    }
+
+    /*
+    private ImageIcon resizeImage(ImageIcon image) {
+	int height = image.getIconHeight();
+	int width = image.getIconWidth();
+	Image tempimg = image.getImage(); // transform it
+	Image newimg = tempimg.getScaledInstance(width / 2, height / 2, java.awt.Image.SCALE_SMOOTH); // scale it the smooth way
+	return new ImageIcon(newimg);
+    }
+
+    private void importPhotos() {
+	String currentDirectory;
+	File file = new File(".");
+	currentDirectory = file.getAbsolutePath();
+
+    }
+
+     */
+    @Override
+    public void actionPerformed(ActionEvent evt) {
+	System.out.println("Action?");
+    }
+
+    private void prevButtonAction() {
+	if (currIndex > 0) {
+	    currIndex--;
+	    if (currIndex < maxIndex) {
+		nextButton.setEnabled(true);
+	    }
+	    if (currIndex <= 0) {
+		prevButton.setEnabled(false);
+	    }
+	    try {
+		photo = db.getNewPhoto(currIndex);
+		setPhoto(photo);
+	    }
+	    catch (SQLException e) {
+		System.err.println("SQL error on prev button");
+	    }
+
+	}
+
+    }
+
+    private void nextButtonAction() {
+	if (currIndex < maxIndex) {
+	    currIndex++;
+	    if (currIndex == 1) {
+		prevButton.setEnabled(true);
+	    }
+	    else if (currIndex >= maxIndex) {
+		nextButton.setEnabled(false);
+	    }
+	    try {
+		photo = db.getNewPhoto(currIndex);
+		setPhoto(photo);
+	    }
+	    catch (SQLException e) {
+		System.err.println("SQL error on next button");
+	    }
+
+	}
+
+    }
+
+    private void setPhoto(databasePhoto newPhoto) {
+
+	ImageIcon image = new ImageIcon(newPhoto.imageArray);
+	imageLabel.setIcon(image);
+	descriptionTextArea.setText(newPhoto.description);
+	dateTextField.setText(newPhoto.date);
+
+    }
+
+    @Override
+    public Dimension getMinimumSize() {
+	return new Dimension(500, 500);
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+	return new Dimension(750, 750);
+    }
+
+    public static void main(String[] args) {
+
+	JFrame frame = new photoViewerGUI();
+
+	frame.pack();
+
+	frame.setVisible(true);
+
+	System.out.println("Program begins...");
+
+    }
+
+}
